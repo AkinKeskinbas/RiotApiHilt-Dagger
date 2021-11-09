184aca83dd7c01e06398f4fe6b8f2e0c26fc40c9
Akin-junior <keskinbas13@gmail.com>
2021-11-10 02:02:53 +0300

    Navigation Add Home Rc item add HomeAdapter Add Data set with Rc

---
+++ app/src/main/java/com/akin/leagueoflegends/ui/adapters/HomeAdapter.kt
@@ -0,0 +1,49 @@
+package com.akin.leagueoflegends.ui.adapters
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.recyclerview.widget.RecyclerView
+import com.akin.leagueoflegends.data.models.allcharacternameanddata.CharacterModel
+import com.akin.leagueoflegends.databinding.FragmentHomeBinding
+import com.akin.leagueoflegends.databinding.HomeItemBinding
+import com.akin.leagueoflegends.util.loadString
+import com.akin.leagueoflegends.util.makePlaceHolder
+
+class HomeAdapter : RecyclerView.Adapter<HomeAdapter.HomeViewHolder>() {
+    private var heroList: List<CharacterModel> = mutableListOf()
+    private var heroImageList: List<String> = mutableListOf()
+
+    class HomeViewHolder(val binding: HomeItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeAdapter.HomeViewHolder {
+        val binding =
+            HomeItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return HomeViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: HomeAdapter.HomeViewHolder, position: Int) {
+        holder.binding.apply {
+            val positionedHeroList = heroList[position]
+            val positionedHeroImageList = heroImageList[position]
+            nameText.text = positionedHeroList.name
+            imageHome.loadString(positionedHeroImageList, makePlaceHolder(holder.itemView.context))
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return heroList.size
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun loadCollectionsData(items: List<CharacterModel>, images:List<String>) {
+        this.heroList = items
+        this.heroImageList = images
+        notifyDataSetChanged()
+    }
+}
---
+++ app/src/main/java/com/akin/leagueoflegends/ui/fragment/DetailFragment.kt
@@ -0,0 +1,25 @@
+package com.akin.leagueoflegends.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.akin.leagueoflegends.R
+import com.akin.leagueoflegends.databinding.FragmentDetailBinding
+import com.akin.leagueoflegends.ui.base.BaseFragment
+
+
+class DetailFragment : BaseFragment<FragmentDetailBinding>(FragmentDetailBinding:: inflate) {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
